d27be47dddcd0f84b1af09881865d58c46a4a27a
Ryan Nkansah <39705812+ryan2068@users.noreply.github.com>
2023-09-14 15:34:17 -0400

    Update webscript.js

--- webscript.js
+++ webscript.js
@@ -2,16 +2,37 @@ form = document.querySelector("form")
 firstName = document.querySelector("#first-name")
 lastName = document.querySelector("#last-name")
 
+cardContainer = document.createElement("div")
+cardContainer.classList.add("card-container")
+document.body.appendChild(cardContainer)
+
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault() 
     card = document.createElement("div")
-    card.textContent = `${firstName.value} ${lastName.value}`
-    document.body.append(card)
-    
-    
-    
+    card.classList.add("card")
+    card.textContent = harryPotter.info()
+    cardContainer.appendChild(card)
 })
+
+library = []
+
+function Book(title, author, pages) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.info = function() {
+        return `${this.title}, ${this.author}, ${this.pages}`
+    }
+}
+
+harryPotter = new Book('harry potter', 'JK Rowling','272')
+
+library.push(harryPotter)
+
+
+
 
 
 
